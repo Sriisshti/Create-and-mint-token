c7cc922103b53f7844c721fd268ed71c670580d5
Sriisshti <138436340+Sriisshti@users.noreply.github.com>
2024-06-12 21:58:22 +0530

    Add files via upload

---
+++ SrishtiToken.sol
@@ -0,0 +1,28 @@
+pragma solidity ^0.8.0;
+// SPDX-License-Identifier: MIT
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract SrishtiToken is ERC20 {
+    address admin;
+
+    constructor() ERC20("Srishti", "SR") {
+        admin = msg.sender;
+        _mint(address(this), 100 * 10 ** decimals());
+    }
+
+    modifier onlyAdmin() {
+        require(msg.sender == admin, "This does not belongs to Owner");
+        _;
+    }
+
+    function createTokens(address recipient, uint256 quantity) public onlyAdmin {
+        uint balance = balanceOf(address(this));
+        require(balance >= quantity, "Not sufficient balance");
+        _transfer(address(this), recipient, quantity);
+    }
+
+    function destroyTokens(uint256 amount) public {
+        _burn(msg.sender, amount);
+    }
+}
